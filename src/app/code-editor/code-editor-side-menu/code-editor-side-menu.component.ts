a44b10dae32cc0e85b7d8f0acb497f32a7c5856a
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-06-11 11:50:29 +0300

    Created side menu code editor

---
+++ src/app/code-editor/code-editor-side-menu/code-editor-side-menu.component.ts
@@ -0,0 +1,47 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: "app-code-editor-side-menu",
+  templateUrl: "./code-editor-side-menu.component.html",
+  styleUrls: ["./code-editor-side-menu.component.scss"],
+  animations: [
+    trigger("openClose", [
+      state("open", style({})),
+      state(
+        "close",
+        style({
+          "flex-grow": "0",
+          "padding-left": "0px",
+          "padding-right": "0px",
+        })
+      ),
+      transition("open => close", [animate("0.3s ease-in-out")]),
+      transition("close => open", [animate("0.3s ease-in-out")]),
+    ]),
+  ],
+})
+export class CodeEditorSideMenuComponent implements OnInit {
+  leftArrow = faChevronLeft;
+  rightArrow = faChevronRight;
+
+  isOpen = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}
--- src/app/code-editor/code-editor.module.ts
+++ src/app/code-editor/code-editor.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { SharedModule } from "../shared/shared.module";
+import { CodeEditorSideMenuComponent } from "./code-editor-side-menu/code-editor-side-menu.component";
 import { CodeEditorComponent } from "./code-editor/code-editor.component";
 import { CodeEditorBaseComponent } from "./components/code-editor-base/code-editor-base.component";
 
 @NgModule({
-  declarations: [CodeEditorComponent, CodeEditorBaseComponent],
-  imports: [SharedModule],
-  exports: [CodeEditorComponent],
+  declarations: [
+    CodeEditorComponent,
+    CodeEditorBaseComponent,
+    CodeEditorSideMenuComponent,
+  ],
+  imports: [SharedModule, FontAwesomeModule],
+  exports: [CodeEditorComponent, CodeEditorSideMenuComponent],
 })
 export class CodeEditorModule {}
