686c724f7ad7d86f44224cf2b7324d0847d6c40b
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-06-17 02:35:40 +0300

    Added Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CodeEditorModule } from "./code-editor/code-editor.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +16,7 @@ import { CodeEditorModule } from "./code-editor/code-editor.module";
     AppRoutingModule,
     CodeEditorModule,
     HammerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
